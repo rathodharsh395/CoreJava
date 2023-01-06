ebf2dc8bbcd23ddf02d0ec88b4d2983d5f77b441
Harish Rathod <rathodharsh395@gmail.com>
2023-01-06 11:30:12 +0530

    new file

--- src/HelloJavaProgram.java
+++ src/HelloJavaProgram.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class HelloJavaProgram {
+public class HelloJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
 }
