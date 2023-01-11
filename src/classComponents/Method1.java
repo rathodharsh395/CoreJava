bea3bebe51a0b9c5e4ab4bc7bfdf529d9cc48903
Harish Rathod <rathodharsh395@gmail.com>
2023-01-11 11:47:06 +0530

    Method Assignment change less than equal to

--- src/classComponents/Method1.java
+++ src/classComponents/Method1.java
@@ -15,24 +15,23 @@ public class Method1
     }
         int  maxNum(int number1,int number2, int number3)
         {
-            if (number1 > number2 && number1 > number3) {
+            if (number1 >= number2 && number1 > number3) {
                 return number1;
             }
             else {
-                 if (number2 > number1 && number2 > number3) {
+                 if (number2 > number1 && number2 >= number3) {
                     return number2;
                  } else {
                     return number3;
                 }
              }
         }
-
         int  minNum(int number1,int number2, int number3){
-                if(number1 < number2 && number1 < number3)
+                if(number1 <= number2 && number1 < number3)
                 { return number1 ;
                 }
                 else{
-                    if(number2 < number1 && number2 < number3)
+                    if(number2 < number1 && number2 <= number3)
                     { return number2;
                     }
                     else{
