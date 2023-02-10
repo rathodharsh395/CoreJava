7f57665de14cf30c8f22e205c724dc037c3d894c
Harish Rathod <rathodharsh395@gmail.com>
2023-02-10 23:16:24 +0530

    Array demos 2

---
+++ src/Array/Demo3LinearSearch.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Demo3LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {11,12,1,54,59};
+        int element = 54;
+
+        for(int index=0; index<array.length;index++){
+            if(element == array[index]){
+                System.out.println("element is present at\t"+index);
+            }
+        }
+    }
+}
---
+++ src/Array/Demo4EvenNumbers.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Demo4EvenNumbers {
+    public static void main(String[] args) {
+        int [] arry = {11,2,34,54,59};
+
+        for(int index=0; index<arry.length;index++){
+           if(arry[index]%2==0){
+               System.out.println(arry[index]);
+           }
+        }
+    }
+}
